b02690a298b415a753647447d9dfb6002a61705b
caoliu <tsaol@outlook.com>
2024-06-14 18:07:25 +0800

    Update musicgen_app.py

--- demos/musicgen_app.py
+++ demos/musicgen_app.py
@@ -255,6 +255,8 @@ def ui_full(launch_kwargs):
                     with gr.Column():
                         radio = gr.Radio(["file", "mic"], value="file",
                                          label="Condition on a melody (optional) File or Mic")
+                        melody = gr.Audio(source="upload", type="numpy", label="File",
+                                          interactive=True, elem_id="melody-input"，visible=False)
                 with gr.Row():
                     submit = gr.Button("Submit")
                     # Adapted from https://github.com/rkfg/audiocraft/blob/long/app.py, MIT license.
@@ -354,6 +356,8 @@ def ui_batched(launch_kwargs):
                     with gr.Column():
                         radio = gr.Radio(["file", "mic"], value="file",
                                          label="Condition on a melody (optional) File or Mic")
+                        melody = gr.Audio(source="upload", type="numpy", label="File",
+                                          interactive=True, elem_id="melody-input"，visible=False)
                 with gr.Row():
                     submit = gr.Button("Generate")
             with gr.Column():
